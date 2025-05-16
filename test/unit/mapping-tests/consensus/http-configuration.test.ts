986edc3a82d2be5381b6b0e45cfe92e6bc538425
Eniko Nagy <4188977+eenagy@users.noreply.github.com>
2025-05-16 08:37:06 +0200

    feat: add consensus rest api flags

--- test/unit/mapping-tests/consensus/http-configuration.test.ts
+++ test/unit/mapping-tests/consensus/http-configuration.test.ts
@@ -4,12 +4,12 @@ import { ConsensusClientName } from '@/lib/types';
 import { testConfig } from '../../preset-tests/network-preset.test-helper';
 import SchemaUtils from '@/utils/schema';
 
-describe.skip('Consensus Client HTTP Configuration Tests', () => {
+describe('Consensus Client HTTP Configuration Tests', () => {
   let registry: CommandClientRegistry;
   const schemaUtils = new SchemaUtils('');
   const consensusClients: ConsensusClientName[] = [
     'lighthouse',
-    "lodestar",
+    'lodestar',
     'nimbus-eth2',
     'prysm',
     'teku',
@@ -21,7 +21,7 @@ describe.skip('Consensus Client HTTP Configuration Tests', () => {
 
   consensusClients.forEach(client => {
     describe(`${client} http configuration`, () => {
-      it('should not include any http flags when http is disabled', () => {
+      it('should include http flags when http is enabled', () => {
         const config = schemaUtils.deepMerge(testConfig, {
           common: {
             engine: {
@@ -30,7 +30,12 @@ describe.skip('Consensus Client HTTP Configuration Tests', () => {
           },
           consensus: {
             http: {
-              enabled: true
+              enabled: true,
+              port: 8545,
+              api: ['eth', 'net', 'web3'],
+              address: 'localhost',
+              allowlist: ['*'],
+              vhosts: ['*'],
             },
             client: {
               name: client,
@@ -41,14 +46,117 @@ describe.skip('Consensus Client HTTP Configuration Tests', () => {
 
         const scriptContent = registry.getScriptContent(client, config);
         const scriptString = scriptContent.toString();
-
+        const httpPort = config.consensus.http.port;
+        const httpApi = config.consensus.http.api.join(',');
+        const httpAddress = config.consensus.http.address;
+        const httpAllowlist = config.consensus.http.allowlist.join(',');
+        const httpVhosts = config.consensus.http.vhosts.join(',');
         switch (client) {
           case 'lighthouse':
+            expect(scriptString).to.include('--http');
+            expect(scriptString).to.include(`--http-port ${httpPort}`);
+            expect(scriptString).to.include(`--http-address ${httpAddress}`);
+            expect(scriptString).to.include(`--http-allow-origin "${httpAllowlist}"`);
+            // TODO: modules are not present in flags 
+            // TODO: vhosts are not present in flags
+            break;
           case 'lodestar':
+            expect(scriptString).to.include('--http');
+            expect(scriptString).to.include(`--http.port ${httpPort}`);
+            expect(scriptString).to.include(`--http.corsdomain "${httpAllowlist}"`);
+            expect(scriptString).to.include(`--http.api "${httpApi}"`);
+            expect(scriptString).to.include(`--http.addr ${httpAddress}`);
+            expect(scriptString).to.include(`--http.vhosts "${httpVhosts}"`);
+            // TODO: why does lodestar has two such flags? vhosts and corsdomain
+            break;
           case 'nimbus-eth2':
+            expect(scriptString).to.include('--rest');
+            expect(scriptString).to.include(`--rest-port ${httpPort}`);
+            expect(scriptString).to.include(`--rest-address ${httpAddress}`);
+            expect(scriptString).to.include(`--rest-allow-origin "${httpAllowlist}"`);
+            // TODO: modules are not present in flags 
+            // TODO: vhosts are not present in flags
+            break;
           case 'prysm':
+            expect(scriptString).to.include(`--http-port ${httpPort}`);
+            expect(scriptString).to.include(`--http-cors-domain "${httpAllowlist}"`);
+            expect(scriptString).to.include(`--http-host ${httpAddress}`);
+            expect(scriptString).to.include(`--http-modules ${httpApi}`);
+            break;
           case 'teku':
-            expect(false).to.be.true;
+            expect(scriptString).to.include(`--rest-api-enabled`);
+            expect(scriptString).to.include(`--rest-api-interface=${httpAddress}`);
+            expect(scriptString).to.include(`--rest-api-port=${httpPort}`);
+            // TODO: why does teku has two such flags?
+            expect(scriptString).to.include(`--rest-api-host-allowlist="${httpAllowlist}"`);
+            expect(scriptString).to.include(`--rest-api-cors-origins="${httpAllowlist}"`);
+            break;
+        }
+      });
+
+      it('should not include http flags when http is disabled', () => {
+        const config = schemaUtils.deepMerge(testConfig, {
+          common: {
+            engine: {
+              enabled: false
+            }
+          },
+          consensus: {
+            http: {
+              enabled: false,
+              port: 8545,
+              api: ['eth', 'net', 'web3'],
+              address: 'localhost',
+              allowlist: ['*'],
+              vhosts: ['*'],
+            },
+            client: {
+              name: client,
+              version: ''
+            },
+          }
+        });
+
+        const scriptContent = registry.getScriptContent(client, config);
+        const scriptString = scriptContent.toString();
+        const httpPort = config.consensus.http.port;
+        const httpApi = config.consensus.http.api.join(',');
+        const httpAddress = config.consensus.http.address;
+        const httpAllowlist = config.consensus.http.allowlist.join(',');
+        const httpVhosts = config.consensus.http.vhosts.join(',');
+        switch (client) {
+          case 'lighthouse':
+            expect(scriptString).to.not.include('--http');
+            expect(scriptString).to.not.include(`--http-port ${httpPort}`);
+            expect(scriptString).to.not.include(`--http-address ${httpAddress}`);
+            expect(scriptString).to.not.include(`--http-allow-origin "${httpAllowlist}"`);
+            break;
+          case 'lodestar':
+            expect(scriptString).to.not.include('--http');
+            expect(scriptString).to.not.include(`--http.port ${httpPort}`);
+            expect(scriptString).to.not.include(`--http.corsdomain "${httpAllowlist}"`);
+            expect(scriptString).to.not.include(`--http.api "${httpApi}"`);
+            expect(scriptString).to.not.include(`--http.addr ${httpAddress}`);
+            expect(scriptString).to.not.include(`--http.vhosts "${httpVhosts}"`);
+            break;
+          case 'nimbus-eth2':
+            expect(scriptString).to.not.include('--rest');
+            expect(scriptString).to.not.include(`--rest-port ${httpPort}`);
+            expect(scriptString).to.not.include(`--rest-address ${httpAddress}`);
+            expect(scriptString).to.not.include(`--rest-allow-origin "${httpAllowlist}"`);
+            break;
+          case 'prysm':
+            expect(scriptString).to.not.include(`--http-port ${httpPort}`);
+            expect(scriptString).to.not.include(`--http-cors-domain "${httpAllowlist}"`);
+            expect(scriptString).to.not.include(`--http-host ${httpAddress}`);
+            expect(scriptString).to.not.include(`--http-modules ${httpApi}`);
+            break;
+          case 'teku':
+            expect(scriptString).to.not.include(`--rest-api-enabled`);
+            expect(scriptString).to.not.include(`--rest-api-interface=${httpAddress}`);
+            expect(scriptString).to.not.include(`--rest-api-port=${httpPort}`);
+            expect(scriptString).to.not.include(`--rest-api-host-allowlist="${httpAllowlist}"`);
+            expect(scriptString).to.not.include(`--rest-api-cors-origins="${httpAllowlist}"`);
             break;
         }
       });
